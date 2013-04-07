a47e07124a414830b941375ba40bd92d4ebb6a61
Stephen Li <lw_stephen@126.com>
2013-04-07 13:52:31 +0800

    new file:   .gitignore 	new file:   Gemfile 	new file:   Gemfile.lock 	new file:   README.rdoc 	new file:   Rakefile 	new file:   app/assets/images/rails.png 	new file:   app/assets/javascripts/application.js 	new file:   app/assets/javascripts/products.js.coffee 	new file:   app/assets/stylesheets/application.css 	new file:   app/assets/stylesheets/products.css.scss 	new file:   app/assets/stylesheets/scaffolds.css.scss 	new file:   app/controllers/application_controller.rb 	new file:   app/controllers/products_controller.rb 	new file:   app/helpers/application_helper.rb 	new file:   app/helpers/products_helper.rb 	new file:   app/mailers/.gitkeep 	new file:   app/models/.gitkeep 	new file:   app/models/product.rb 	new file:   app/views/layouts/application.html.erb 	new file:   app/views/products/_form.html.erb 	new file:   app/views/products/edit.html.erb 	new file:   app/views/products/index-1.html.erb 	new file:   app/views/products/index.html.erb 	new file:   app/views/products/new.html.erb 	new file:   app/views/products/show.html.erb 	new file:   config.ru 	new file:   config/application.rb 	new file:   config/boot.rb 	new file:   config/database.yml 	new file:   config/environment.rb 	new file:   config/environments/development.rb 	new file:   config/environments/production.rb 	new file:   config/environments/test.rb 	new file:   config/initializers/backtrace_silencers.rb 	new file:   config/initializers/inflections.rb 	new file:   config/initializers/mime_types.rb 	new file:   config/initializers/secret_token.rb 	new file:   config/initializers/session_store.rb 	new file:   config/initializers/wrap_parameters.rb 	new file:   config/locales/en.yml 	new file:   config/routes.rb 	new file:   db/migrate/20130405065946_create_products.rb 	new file:   db/schema.rb 	new file:   db/seeds.rb 	new file:   doc/README_FOR_APP 	new file:   lib/assets/.gitkeep 	new file:   lib/tasks/.gitkeep 	new file:   log/.gitkeep 	new file:   public/404.html 	new file:   public/422.html 	new file:   public/500.html 	new file:   public/favicon.ico 	new file:   public/images/debug.jpg 	new file:   public/images/logo.png 	new file:   public/images/rails.png 	new file:   public/images/rtp.jpg 	new file:   public/images/ruby.jpg 	new file:   public/images/wd4d.jpg 	new file:   public/index.html 	new file:   public/robots.txt 	new file:   public/stylesheets/depot.css 	new file:   script/rails 	new file:   test/fixtures/.gitkeep 	new file:   test/fixtures/products.yml 	new file:   test/functional/.gitkeep 	new file:   test/functional/products_controller_test.rb 	new file:   test/integration/.gitkeep 	new file:   test/performance/browsing_test.rb 	new file:   test/test_helper.rb 	new file:   test/unit/.gitkeep 	new file:   test/unit/helpers/products_helper_test.rb 	new file:   test/unit/product_test.rb 	new file:   vendor/assets/javascripts/.gitkeep 	new file:   vendor/assets/stylesheets/.gitkeep 	new file:   vendor/plugins/.gitkeep

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '86b9fed986a11b9418603b99513b402c7beaa96bfa982f9b1c574d7da029b911f6caa406840aa7414a403bc8a36155b2494cb33d12e9f274f8f7cd323275113d'
